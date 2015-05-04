52903bd6648550dd5f1ef88601393e8250138ec3
lassebunk <lassebunk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-04 19:15:41 +0000

    Initial commit
    git-svn-id: https://plugins.svn.wordpress.org/screenshot-generator/trunk@1153188 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin.php
@@ -0,0 +1,270 @@
+<?php
+add_action( 'admin_menu', 'scrgen_add_admin_menu' );
+add_action( 'admin_init', 'scrgen_settings_init' );
+add_action( 'admin_action_scrgen-regenerate', 'scrgen_admin_regenerate' );
+
+
+function scrgen_add_admin_menu() {
+  add_options_page( 'Screenshot Generator', 'Screenshot Generator', 'manage_options', 'screenshot-generator', 'scrgen_options_page' );
+}
+
+function scrgen_admin_regenerate() {
+  query_posts( 'post_status=publish&post_type=any&posts_per_page=-1' );
+
+  while ( have_posts() ) : the_post();
+    $id = get_the_ID();
+    scrgen_queue_post_update($id);
+  endwhile;
+
+  wp_reset_query();
+  wp_redirect( $_SERVER['HTTP_REFERER'] . '&msg=regenerating' );
+  exit();
+}
+
+function scrgen_settings_init() {
+  register_setting( 'scrgen-settings', 'scrgen_width' );
+  register_setting( 'scrgen-settings', 'scrgen_height' );
+  register_setting( 'scrgen-settings', 'scrgen_enable_cropping' );
+  register_setting( 'scrgen-settings', 'scrgen_crop_left' );
+  register_setting( 'scrgen-settings', 'scrgen_crop_top' );
+  register_setting( 'scrgen-settings', 'scrgen_crop_width' );
+  register_setting( 'scrgen-settings', 'scrgen_crop_height' );
+  register_setting( 'scrgen-settings', 'scrgen_social_strategy' );
+
+  // Size section
+
+  add_settings_section(
+    'scrgen_size_section',
+    __( 'Screenshot size', 'scrgen' ),
+    '',
+    'scrgen-settings'
+  );
+
+  add_settings_field(
+    'scrgen_width',
+    __( 'Width', 'scrgen' ),
+    'scrgen_width_render',
+    'scrgen-settings',
+    'scrgen_size_section'
+  );
+
+  add_settings_field(
+    'scrgen_height',
+    __( 'Height', 'scrgen' ),
+    'scrgen_height_render',
+    'scrgen-settings',
+    'scrgen_size_section'
+  );
+
+  // Cropping section
+
+  add_settings_section(
+    'scrgen_cropping_section',
+    __( 'Cropping', 'scrgen' ),
+    '',
+    'scrgen-settings'
+  );
+
+  add_settings_field(
+    'scrgen_enable_cropping',
+    __( 'Enable cropping', 'scrgen' ),
+    'scrgen_enable_cropping_render',
+    'scrgen-settings',
+    'scrgen_cropping_section'
+  );
+
+  add_settings_field(
+    'scrgen_crop_left',
+    __( 'Left', 'scrgen' ),
+    'scrgen_crop_left_render',
+    'scrgen-settings',
+    'scrgen_cropping_section'
+  );
+
+  add_settings_field(
+    'scrgen_crop_top',
+    __( 'Top', 'scrgen' ),
+    'scrgen_crop_top_render',
+    'scrgen-settings',
+    'scrgen_cropping_section'
+  );
+
+  add_settings_field(
+    'scrgen_crop_width',
+    __( 'Width', 'scrgen' ),
+    'scrgen_crop_width_render',
+    'scrgen-settings',
+    'scrgen_cropping_section'
+  );
+
+  add_settings_field(
+    'scrgen_crop_height',
+    __( 'Height', 'scrgen' ),
+    'scrgen_crop_height_render',
+    'scrgen-settings',
+    'scrgen_cropping_section'
+  );
+
+  // Social section
+
+  add_settings_section(
+    'scrgen_social_section',
+    __( 'Social settings', 'scrgen' ),
+    'scrgen_social_section',
+    'scrgen-settings'
+  );
+
+  add_settings_field(
+    'scrgen_social_strategy',
+    __( 'Enable social screenshots', 'scrgen' ),
+    'scrgen_social_strategy_render',
+    'scrgen-settings',
+    'scrgen_social_section'
+  );
+
+  // Manual section
+
+  add_settings_section(
+    'scrgen_manual_section',
+    __( 'Displaying screenshots manually', 'scrgen' ),
+    'scrgen_manual_section',
+    'scrgen-settings'
+  );
+
+  // Regenerate section
+
+  add_settings_section(
+    'scrgen_regenerate_section',
+    __( 'Regenerate screenshots', 'scrgen' ),
+    'scrgen_regenerate_section',
+    'scrgen-settings'
+  );
+}
+
+
+function scrgen_width_render() {
+  ?>
+  <input type='number' name='scrgen_width' value='<?php echo scrgen_setting('width'); ?>'> px
+  <?php
+}
+
+
+function scrgen_height_render() {
+  ?>
+  <input type='number' name='scrgen_height' value='<?php echo scrgen_setting('height'); ?>'> px
+  <?php
+}
+
+function scrgen_enable_cropping_render() {
+  ?>
+  <label>
+    <input type='checkbox' name='scrgen_enable_cropping' <?php checked(scrgen_setting('enable_cropping'), 1); ?> value='1'>
+    Crop screenshots using the below settings.
+  </label>
+  <?php
+}
+
+
+function scrgen_crop_left_render() {
+  ?>
+  <input type='number' name='scrgen_crop_left' value='<?php echo scrgen_setting('crop_left'); ?>'> px
+  <?php
+}
+
+
+function scrgen_crop_top_render() {
+  ?>
+  <input type='number' name='scrgen_crop_top' value='<?php echo scrgen_setting('crop_top'); ?>'> px
+  <?php
+}
+
+
+function scrgen_crop_width_render() {
+  ?>
+  <input type='number' name='scrgen_crop_width' value='<?php echo scrgen_setting('crop_width'); ?>'> px
+  <?php
+}
+
+
+function scrgen_crop_height_render() {
+  ?>
+  <input type='number' name='scrgen_crop_height' value='<?php echo scrgen_setting('crop_height'); ?>'> px
+  <?php
+}
+
+
+function scrgen_social_strategy_render() {
+  ?>
+  <label>
+    <input type='radio' name='scrgen_social_strategy' <?php checked(scrgen_setting('social_strategy'), 'missing'); ?> value='missing'>
+    Add screenshots for social media when no featured image is available.
+  </label>
+  <br />
+  <label>
+    <input type='radio' name='scrgen_social_strategy' <?php checked(scrgen_setting('social_strategy'), 'always'); ?> value='always'>
+    Always add screenshots for social media.
+  </label>
+  <br />
+  <label>
+    <input type='radio' name='scrgen_social_strategy' <?php checked(scrgen_setting('social_strategy'), ''); ?> value=''>
+    Never add screenshots for social media.
+  </label>
+  <?php
+}
+
+function scrgen_social_section() {
+  echo __( '<code>og:image</code> and <code>twitter:image:src</code> can be added automatically to the HTML <code>&lt;head&gt;</code>.', 'scrgen' );
+}
+
+function scrgen_manual_section() {
+
+  echo __( 'You can display screenshots manually inside posts by inserting: <pre>&lt;?php echo scrgen_screenshot(); ?&gt;</pre>', 'scrgen' );
+
+}
+
+function scrgen_regenerate_section() {
+  ?>
+  <p>
+    You can regenerate all screenshots if you have changed your settings, your site layout, or if you are using the plugin for the first time.
+  </p>
+  <p>
+    <a href="<?php echo admin_url( 'admin.php'); ?>?action=scrgen-regenerate" onclick="return confirm('Are you sure you want to regenerate all screenshots?');">Regenerate all screenshots</a> <em>(you will be asked to confirm)</em>
+  </p>
+  <?php
+}
+
+function scrgen_options_page() {
+  ?>
+  <form action='options.php' method='post'>
+   
+    <h2>Screenshot Generator</h2>
+
+    <p>
+      Screenshot Generator takes automatic screenshots of posts when they are updated.<br />
+      These are saved to <code>wp-content/screenshots</code>.
+    </p>
+
+    <?php if (scrgen_phantomjs() == false) { ?>
+    <div style="background: #faa; color: #700; padding: 10px 15px; border: 1px solid #c00; margin-right: 20px;">
+      The PhantomJS binary (<code>phantomjs</code>) could not be found. Screenshot Generator uses PhantomJS to generate screenshots.<br />
+      Please see the <a href="https://github.com/lassebunk/screenshot-generator#installation" target="_blank">installation instructions</a> for information on how to fix this.
+    </div>
+    <?php } ?>
+   
+    <?php if (isset($_REQUEST['msg']) && $_REQUEST['msg'] == 'regenerating') { ?>
+    <div style="background: #ada; color: #050; padding: 10px 15px; border: 1px solid #090; margin-right: 20px;">
+      Screenshots are now being regenerated in the background, and will be available when done.<br >
+      You are welcome to navigate away from this page.
+    </div>
+    <?php } ?>
+
+    <?php
+    settings_fields( 'scrgen-settings' );
+    do_settings_sections( 'scrgen-settings' );
+    submit_button();
+    ?>
+   
+  </form>
+  <?php
+
+}
---
+++ screenshot-generator.php
@@ -0,0 +1,169 @@
+<?php
+/**
+ * Plugin Name: Screenshot Generator
+ * Plugin URI: https://github.com/lassebunk/screenshot-generator
+ * Description: Take automatic screenshots of posts when they are updated.
+ * Version: 0.1.0
+ * Author: Lasse Bunk
+ * Author URI: https://github.com/lassebunk
+ * License: GPLv2 or later
+ */
+
+function scrgen_defaults() {
+  return array(
+    'width'           => '1024',
+    'height'          => '768',
+    'enable_cropping' => 1,
+    'crop_left'       => 0,
+    'crop_top'        => 0,
+    'crop_width'      => 1024,
+    'crop_height'     => 768,
+    'social_strategy' => 'missing'
+  );
+}
+
+function scrgen_setting($key) {
+  return get_option("scrgen_{$key}", scrgen_defaults()[$key]);
+}
+
+function scrgen_generate_post_screenshot($post_id) {
+  // Workaround bug: http://premium.wpmudev.org/forums/topic/snapshot-faltal-error-on-get_home_path
+  if (!function_exists('get_home_path' )) require_once( dirname(__FILE__) . '/../../../wp-admin/includes/file.php' );
+
+  $url = get_permalink($post_id);
+  $url_segs = parse_url($url);
+
+  $phantomjs = scrgen_phantomjs();
+  if (empty($phantomjs)) {
+    error_log('The phantomjs binary was not found. Make sure it is in your PHP\'s PATH or set the PHANTOMJS constant to its path.');
+    return;
+  }
+
+  $relative_path = 'wp-content/screenshots/' . $post_id . '.jpg';
+  $image_path = get_home_path() . $relative_path;
+  $image_url = get_home_url() . '/' . $relative_path;
+
+  $job_id = uniqid();
+  $job_path = "/tmp/{$job_id}.js";
+  $tmp_path = "/tmp/{$job_id}.jpg";
+
+  $width           = scrgen_setting('width');
+  $height          = scrgen_setting('height');
+  $enable_cropping = scrgen_setting('enable_cropping');
+  $crop_left       = scrgen_setting('crop_left');
+  $crop_top        = scrgen_setting('crop_top');
+  $crop_width      = scrgen_setting('crop_width');
+  $crop_height     = scrgen_setting('crop_height');
+
+  $url = strip_tags($url);
+  $url = str_replace(';', '', $url);
+  $url = str_replace('"', '', $url);
+  $url = str_replace('\'', '/', $url);
+  $url = str_replace('<?', '', $url);
+  $url = str_replace('<?', '', $url);
+  $url = str_replace('\077', ' ', $url);
+
+  $url = escapeshellcmd($url);
+  $src = "
+
+  var page = require('webpage').create();
+
+  page.viewportSize = { width: {$width}, height: {$height} };
+
+  ";
+
+  if ($enable_cropping) {
+      $src .= "page.clipRect = { top: {$crop_top}, left: {$crop_left}, width: {$crop_width}, height: {$crop_height} };";
+  }
+
+  $src .= "
+
+  page.open('{$url}', function () {
+      page.render('{$tmp_path}');
+      phantom.exit();
+  });
+
+
+  ";
+
+  file_put_contents($job_path, $src);
+
+  $exec = $phantomjs . ' ' . $job_path;
+  $escaped_command = escapeshellcmd($exec);
+
+  exec($escaped_command);
+
+  if (is_file($tmp_path)) {
+      rename($tmp_path, $image_path);
+      do_action('scrgen_post_screenshot_generated', $post_id, $image_url);
+  }
+}
+
+function scrgen_queue_post_update($post_id) {
+  wp_schedule_single_event(time(), 'scrgen_update_post_screenshot', array($post_id));
+}
+
+function scrgen_update_post_meta($post_id, $screenshot_url) {
+  update_post_meta($post_id, '_scrgen_screenshot', $screenshot_url);
+}
+
+function scrgen_phantomjs() {
+  $path = exec('which phantomjs');
+
+  if (!empty($path)) {
+    return $path;
+  } elseif (defined('PHANTOMJS')) {
+    return PHANTOMJS;
+  }
+}
+
+function scrgen_screenshot() {
+  $post_id = get_the_ID();
+  $screenshot = get_post_meta($post_id, '_scrgen_screenshot', true);
+  return $screenshot;
+}
+
+function scrgen_screenshot_if_needed() {
+  $thumbnail_id = get_post_thumbnail_id();
+  $screenshot = scrgen_screenshot();
+
+  if (empty($thumbnail_id) && !empty($screenshot)) {
+    return $screenshot;
+  }
+}
+
+function scrgen_opengraph() {
+  $screenshot = scrgen_screenshot();
+  if (!empty($screenshot)) {
+    echo '<meta property="og:image" content="' . $screenshot . '" />';
+  }
+}
+
+function scrgen_twitter() {
+  $screenshot = scrgen_screenshot();
+  if (!empty($screenshot)) {
+    echo '<meta name="twitter:image:src" content="' . $screenshot . '"/>';
+  }
+}
+
+function scrgen_head() {
+  $strategy = scrgen_setting('social_strategy');
+  if ($strategy == '') return;
+
+  $thumbnail_id = get_post_thumbnail_id();
+
+  if ($strategy == 'always' || ($strategy == 'missing' && empty($thumbnail_id))) {
+    scrgen_opengraph();
+    scrgen_twitter();
+  }
+}
+
+add_action('scrgen_update_post_screenshot', 'scrgen_generate_post_screenshot');
+add_action('scrgen_post_screenshot_generated', 'scrgen_update_post_meta', 10, 2);
+add_action('post_updated', 'scrgen_queue_post_update');
+
+add_action('wp_head', 'scrgen_head', 1);
+
+if (is_admin()) {
+  require_once(dirname(__FILE__) . '/admin.php');
+}
